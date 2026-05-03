c01557750e18daf49befc20cf1e058b34829f9ce
anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>
2026-05-03 11:43:05 +0530

    Add files via upload

--- unzipper/modules/__init__.py
+++ unzipper/modules/__init__.py
@@ -10,88 +10,43 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>   #
 # ===================================================================== #
 
-from time import time
-from re import match, sub
+from time import strftime
 from config import Config
-from aiohttp import ClientSession
-from pyrogram.types import Message
-from aiofiles import open as openfile
-from unzipper.helpers_nexa.utils import progress_for_pyrogram
-from .errors import InvalidContentType, FileTooLarge, InvalidUrl, HttpStatusError
+from gofile2 import Gofile
+from unzipper.database.cloud import GofileDB
 
 
-# Http/Https url regex
-# Added as a seperate variable to import only regex pattern
-dl_regex = ("((http|https)://)(www.)?" +
-            "[a-zA-Z0-9@:%._\\+~#?&//=]" +
-            "{2,256}\\.[a-z]" +
-            "{2,6}\\b([-a-zA-Z0-9@:%" +
-            "._\\+~#?&//=]*)")
-
-
-class Downloader:
+class CloudBackup:
     """
-    Download direct links
+    Utility class to backup extracted files to cloud
+
+    Parameters:
+
+        - `id` - User id
     """
 
-    def __init__(self) -> None:
-        self.gdrive_regex = r"https://drive\.google\.com/file/d/(.*?)/.*?\?usp=sharing"
-        self.dl_regex = ("((http|https)://)(www.)?" +
-                         "[a-zA-Z0-9@:%._\\+~#?&//=]" +
-                         "{2,256}\\.[a-z]" +
-                         "{2,6}\\b([-a-zA-Z0-9@:%" +
-                         "._\\+~#?&//=]*)")
+    def __init__(self, id: int) -> None:
+        self.id = id
+        self.ext_dir = f"{Config.DOWNLOAD_LOCATION}/{self.id}/extracted"
 
-    async def download(self, url: str, path: str, message: Message = None, redirect: bool = False, cont_type: str = "application/", udt: str = "**Trying to Download!** \n"):
+    # Gofile
+    async def gofile_backup(self):
         """
-        Download a file from direct / gdrive link
-
-        Parameters:
-
-            - `url` - Url to the file
-            - `path` - Output path
-            - `message` - Pyrogram Message object
-            - `redirect` - Redirect url
-            - `udt` - Header for the progress bar
+        Backup files to gofile.io
         """
-        if match(self.gdrive_regex, url):
-            gurl = await self._parse_gdrive(url)
-            return await self._from_direct_link(url=gurl, path=path, message=message, redirect=True, cont_type=cont_type, udt=udt)
-        elif match(self.dl_regex, url):
-            return await self._from_direct_link(url=url, path=path, message=message, redirect=redirect, cont_type=cont_type, udt=udt)
-        else:
-            raise InvalidUrl
+        # Gofile client
+        gf = await Gofile.initialize(await self._get_gofile_token())
+        gf_id = await self._create_gofile_folder(gf)
+        links = await gf.upload_folder(self.ext_dir, gf_id)
+        return links[0]["downloadPage"]
 
-    async def _parse_gdrive(self, url: str):
-        return sub(r"https://drive\.google\.com/file/d/(.*?)/.*?\?usp=sharing", r"https://drive.google.com/uc?export=download&id=\1", url)
+    async def _create_gofile_folder(self, client: Gofile):
+        rtfid = (await client.get_account())["rootFolder"]
+        cf = (await client.create_folder(rtfid, "Backup of {} in {}".format(self.id, strftime("%b %d, %Y %l:%M%p"))))["id"]
+        await client.set_option(cf, "public", "true")
+        return cf
 
-    async def _from_direct_link(self, url: str, path: str, message: Message = None, redirect: bool = False, cont_type: str = "application/", udt: str = "**Trying to Download!** \n"):
-        async with ClientSession() as session:
-            async with session.get(url, timeout=None, allow_redirects=redirect) as resp:
-                if resp.status == 200:
-                    pass
-                # Support for temporarily moved resources
-                elif resp.status in (301, 302):
-                    resp = await session.get(url, timeout=None, allow_redirects=True)
-                # Raise HttpStatusError if response status isn't 200
-                else:
-                    raise HttpStatusError
-                # Raise InvalidContentType if the content isn't an archive
-                if not cont_type in resp.content_type:
-                    raise InvalidContentType
-                # Handle content length header
-                total = resp.content_length
-                # Raise FileTooLarge if the content size exceeds Config.MAX_DOWNLOAD_SIZE
-                if total and int(total) > Config.MAX_DOWNLOAD_SIZE:
-                    raise FileTooLarge
-                curr = 0
-                st = time()
-                async with openfile(path, mode="wb") as file:
-                    async for chunk in resp.content.iter_chunked(Config.CHUNK_SIZE):
-                        # Raise FileTooLarge if the content size exceeds Config.MAX_DOWNLOAD_SIZE
-                        if curr > Config.MAX_DOWNLOAD_SIZE:
-                            raise FileTooLarge
-                        await file.write(chunk)
-                        curr += len(chunk)
-                        if message:
-                            await progress_for_pyrogram(curr, total, udt, message, st)
+    async def _get_gofile_token(self):
+        gdb = GofileDB(self.id)
+        gt = await gdb.get_token()
+        return gt if gt else Config.GOFILE_TOKEN
